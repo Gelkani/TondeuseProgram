38f8f610bf49e52d0514c97303a4ae046202ff08
gelkani <elkanighada@gmail.com>
2024-01-30 17:45:07 +0100

    add Treatment block and Test

--- src/main/mowitnow/tondeuse/parserUtilities/ParserData.java
+++ src/main/mowitnow/tondeuse/parserUtilities/ParserData.java
@@ -1,4 +1,31 @@
 package main.mowitnow.tondeuse.parserUtilities;
 
+import main.mowitnow.tondeuse.entities.ParamsTondeuse;
+
 public class ParserData {
+
+    public ParserData() {
+    }
+
+    public static boolean parseTondeuse(String tondeuse){
+        StringBuilder stringBuilder = new StringBuilder("");
+        stringBuilder.append(ParamsTondeuse.Orientation.NORTH.getOrientationCode())
+                .append("|").append(ParamsTondeuse.Orientation.SOUTH.getOrientationCode())
+                .append("|").append(ParamsTondeuse.Orientation.EAST.getOrientationCode())
+                .append("|").append(ParamsTondeuse.Orientation.WEST.getOrientationCode());
+        return tondeuse.matches("(\\d+) (\\d+) (" + stringBuilder.toString()+")");
+    }
+
+    public static boolean parseListInstruction(String instructions){
+        StringBuilder stringBuilder = new StringBuilder("");
+        stringBuilder.append("(").append(ParamsTondeuse.InstructionTondeuse.AVANCER.getInstructionCode())
+                .append("|").append(ParamsTondeuse.InstructionTondeuse.DROITE.getInstructionCode())
+                .append("|").append(ParamsTondeuse.InstructionTondeuse.GAUCHE.getInstructionCode())
+                .append(")+");
+
+        return instructions.matches(stringBuilder.toString());
+    }
+    public static boolean parsePelouse(String pelouse){
+        return pelouse.matches("(\\d+) (\\d+)");
+    }
 }
--- src/main/mowitnow/tondeuse/parserUtilities/ParserTondeuse.java
+++ src/main/mowitnow/tondeuse/parserUtilities/ParserTondeuse.java
@@ -1,4 +1,44 @@
 package main.mowitnow.tondeuse.parserUtilities;
 
 public class ParserTondeuse {
+
+    private String pelouse ;
+    private String tondeuse ;
+    private String instructions ;
+
+    public ParserTondeuse() {
+        this.pelouse = "";
+        this.tondeuse = "";
+        this.instructions= "";
+    }
+
+    public String getPelouse() {
+        return pelouse;
+    }
+
+    public void setPelouse(String pelouse) {
+        this.pelouse = pelouse;
+    }
+
+    public String getTondeuse() {
+        return tondeuse;
+    }
+
+    public void setTondeuse(String tondeuse) {
+        this.tondeuse = tondeuse;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public boolean executeParse(){
+        return ParserData.parseTondeuse(tondeuse)
+                && ParserData.parsePelouse(pelouse)
+                && ParserData.parseListInstruction(instructions);
+    }
 }
